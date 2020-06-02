6ff9c358cd02a40c28b17f030d240b3396e5d3c3
zhichkin <dima_zhichkin@mail.ru>
2020-06-03 00:38:59 +0300

    service broker scripts
    + delete queue added

--- sql/service broker/install-service-broker-database.sql
+++ sql/service broker/install-service-broker-database.sql
@@ -230,6 +230,44 @@ BEGIN
 END;
 GO
 
+-- ================================
+-- Create procedure to delete queue
+-- ================================
+IF OBJECT_ID('dbo.sp_delete_queue', 'P') IS NOT NULL
+BEGIN
+	DROP PROCEDURE [dbo].[sp_delete_queue];
+END;
+GO
+
+CREATE PROCEDURE [dbo].[sp_delete_queue](@name nvarchar(80))
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	DECLARE @queue_name nvarchar(128) = [dbo].[fn_create_queue_name](@name);
+	DECLARE @service_name nvarchar(128) = [dbo].[fn_create_service_name](@name);
+	DECLARE @default_service_name nvarchar(128) = [dbo].[fn_default_service_name]();
+
+	IF (@default_service_name = @queue_name) THROW 50001, N'The default queue can not be droped!', 1;
+	
+	IF EXISTS(SELECT 1 FROM sys.services WHERE name = @service_name)
+	BEGIN
+		EXEC(N'DROP SERVICE [' + @service_name + N'];');
+	END;
+
+	IF EXISTS(SELECT 1 FROM sys.service_queues WHERE name = @queue_name)
+	BEGIN
+		EXEC(N'DROP QUEUE [dbo].[' + @queue_name + N'];');
+	END;
+
+	-- ==============================================
+	-- Receive error message for default local dialog
+	-- ==============================================
+	-- http://schemas.microsoft.com/SQL/ServiceBroker/Error
+	-- 'Remote service has been dropped.' message !
+END;
+GO
+
 -- ===========================================
 -- Create procedure to get local dialog handle
 -- ===========================================
